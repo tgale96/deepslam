d5a7ed80080d7abdb933eb75b9ad2e731ea42e3b
Trevor Gale <gale.t@husky.neu.edu>
2018-03-15 01:17:07 -0400

    Updated to use posenet loss, avergae over samples in MSE loss, and extended network width. Also raised learning rate again. Was able to train to ~86 loss

--- pose_test.py
+++ pose_test.py
@@ -1,3 +1,4 @@
+
 import torch
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
@@ -9,49 +10,53 @@ class PoseNet(torch.nn.Module):
         self.linear1 = torch.nn.Linear(in_dims, hidden_dims)
         self.relu = torch.nn.ReLU(inplace=True)
         self.linear2 = torch.nn.Linear(hidden_dims, out_dims)
-
+        
     def forward(self, x):
         out = self.linear1(x)
         out = self.relu(out)
         out = self.linear2(out)
-        return out
+
+        # PoseNet loss
+        xyz = out[:, 0:3]
+        wpqr = out[:, 3:]
+        norm = torch.norm(wpqr)
+        wpqr_norm = wpqr / norm
+        return xyz, wpqr_norm
 
 # Input dims
-batch_size, in_dims, hidden_dims, out_dims = 32, 480*640*3, 100, 7
-epochs = 1
+batch_size, in_dims, hidden_dims, out_dims = 32, 480*640*3, 1000, 7
+epochs = 100
+beta = 500
 
 # Create the data loader
 dataloader = DataLoader(SlamDataset("data/slam_data.h5", "rgbd_dataset_freiburg1_xyz"),
-                        batch_size = batch_size, shuffle = True, num_workers = 1)
+                        batch_size = batch_size, shuffle = True, num_workers = 0)
 
 # Create the model
 model = PoseNet(in_dims, hidden_dims, out_dims)
 
-criterion = torch.nn.MSELoss(size_average=False)
-optimizer = torch.optim.SGD(model.parameters(), lr=0.000000000000001)
+criterion_xyz = torch.nn.MSELoss(size_average=True)
+criterion_wpqr = torch.nn.MSELoss(size_average=True)
+optimizer = torch.optim.SGD(model.parameters(), lr=0.00000001)
 
 for e in range(epochs):
     for i_batch, batch in enumerate(dataloader):
-        # Run the forward pass
+        # Get the data for this iter
         data = Variable(batch['rgb'])
         target = Variable(batch['pose'])
-        output = model(data)
+        target_xyz = target[:, 0:3]
+        target_wpqr = target[:, 3:]
+
+        # Run the forward pass
+        xyz, wpqr = model(data)
 
         # Calculate and print loss
-        loss = criterion(output, target)
+        loss_xyz = criterion_xyz(xyz, target_xyz)
+        loss_wpqr = beta * criterion_wpqr(wpqr, target_wpqr)
+        loss = sum([loss_xyz, loss_wpqr])
         print(i_batch, loss.data[0])
-        
+
+        # Backprop and update
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-
-
-
-
-
-
-
-
-
-
-
