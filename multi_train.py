15187cabc69ea6bd105c6af6eb3ff4e61e37d3e5
Trevor Gale <gale.t@husky.neu.edu>
2018-04-01 16:57:03 -0700

    Reworked multi-task training script to optimize both losses

--- multi_train.py
+++ multi_train.py
@@ -20,6 +20,7 @@ parser.add_argument("--lr", type=float, default=0.1)
 args = parser.parse_args()
 
 batch_size = 32
+beta = 500
 epochs = 100
 lr = args.lr
 
@@ -43,7 +44,7 @@ if args.tune:
 if args.cuda:
     model = model.cuda()
     
-depth_criterion = nn.MSELoss(size_average=True)
+criterion_depth = nn.MSELoss(size_average=True)
 criterion_xyz = nn.MSELoss(size_average=True)
 criterion_wpqr = nn.MSELoss(size_average=True)
 optimizer = torch.optim.SGD(model.parameters(), lr=lr)
@@ -52,53 +53,81 @@ def train(epoch):
     model.train()
     for batch_idx, batch in enumerate(train_loader):
         # Get the data for this iter
-        data = Variable(batch['rgb'])
-        target = Variable(batch['depth'])
+        data = Variable(batch['rgb_pair'])
+        target_depth = Variable(batch['depth'])
+        target_pose = Variable(batch['pose_diff'])
         if args.cuda:
             data = data.cuda()
-            target = target.cuda()
+            target_depth = target_depth.cuda()
+            target_pose = target_pose.cuda()
             
+        target_xyz = target_pose[:, 0:3]
+        target_wpqr = target_pose[:, 3:]
+
         # Run the forward pass
-        dec = model(data)
+        depth, xyz, wpqr = model(data)
         
-        # Calculate and print loss
-        loss = criterion(dec, target)
+        # Calculate depth loss
+        loss_depth = criterion_depth(depth, target_depth)
+        
+        # Calculate pose loss
+        loss_xyz = criterion_xyz(xyz, target_xyz)
+        loss_wpqr = beta * criterion_wpqr(wpqr, target_wpqr)
+        loss_pose = sum([loss_xyz, loss_wpqr])
         
         # Backprop and update
         optimizer.zero_grad()
-        loss.backward()
+        loss_pose.backward()
+        loss_depth.backward()
         optimizer.step()
 
-        print("Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}".format(
+        print("Train Epoch: {} [{}/{} ({:.0f}%)]\tDepth Loss: {:.6f}\tPose Loss: {:.6f}".format(
             epoch, batch_idx*batch_size, len(train_loader.dataset),
-            100. * batch_idx / len(train_loader), loss.data[0]))
+            100. * batch_idx / len(train_loader), loss_depth.data[0], 
+            loss_pose.data[0]))
     
 def test(epoch):
     model.eval()
-    avg_loss = 0
+    avg_loss_depth = 0
+    avg_loss_xyz = 0
+    avg_loss_wpqr = 0
 
     for batch_idx, batch in enumerate(test_loader):
         # Get the data for this iter
-        data = Variable(batch['rgb'])
-        target = Variable(batch['depth'])
+        data = Variable(batch['rgb_pair'])
+        target_depth = Variable(batch['depth'])
+        target_pose = Variable(batch['pose_diff'])
         if args.cuda:
             data = data.cuda()
-            target = target.cuda()
+            target_depth = target_depth.cuda()
+            target_pose = target_pose.cuda()
             
-        # Run the forward pass
-        dec = model(data)
+        target_xyz = target_pose[:, 0:3]
+        target_wpqr = target_pose[:, 3:]
 
-        # Calculate and print loss
-        loss = criterion(dec, target).data[0]
-        avg_loss += loss
+        # Run the forward pass
+        depth, xyz, wpqr = model(data)
+        
+        # Calculate depth loss
+        loss_depth = criterion_depth(depth, target_depth)
+        
+        # Calculate pose loss
+        loss_xyz = criterion_xyz(xyz, target_xyz)
+        loss_wpqr = beta * criterion_wpqr(wpqr, target_wpqr)
+
+        avg_loss_depth += loss_depth.data[0]
+        avg_loss_xyz += loss_xyz.data[0]
+        avg_loss_wpqr += loss_wpqr.data[0]
     
-    avg_loss /= len(test_loader)
-    print("Test Set:\tAvg. Loss : {:.6f}".format(avg_loss))
+    avg_loss_depth /= len(test_loader)
+    avg_loss_xyz /= len(test_loader)
+    avg_loss_wpqr /= len(test_loader)
+    print("Test Set:\tAvg. Depth Loss: {:.6f}\tAvg. XYZ Loss: {:.6f}\tAvg. WPQR Loss: {:.6f}"
+          .format(avg_loss_depth, avg_loss_xyz, avg_loss_wpqr))
 
     # Save the model state
-    torch.save(model.state_dict(), "mse{}_epoch_{}_lr{}_b{}.pt"
-               .format(avg_loss, epoch, lr, batch_size))
-
+    torch.save(model.state_dict(), "depth{}_xyz{}_wpqr{}_epoch_{}_lr{}_b{}.pt"
+               .format(avg_loss_depth, avg_loss_xyz, avg_loss_wpqr, epoch, lr, batch_size))
     
 for epoch in range(1, epochs+1):
     train(epoch)
